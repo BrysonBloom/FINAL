2fff34e984449f6b467c8f9698e7424f6142f987
Bryson Bloom <brysonbloom@icloud.com>
2023-03-31 10:30:45 -0600

    first commit

---
+++ FINAL/Globals.cs
@@ -0,0 +1,16 @@
+global using System;
+global using System.Data;
+global using System.Collections.Generic;
+global using System.IO;
+global using System.Linq;
+
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Mvc;
+global using System.Threading.Tasks;
+global using Dapper;
+global using CodeWorks.Utils;
+
+// APPLICATION SPECIFIC
+global using FINAL.Repositories;
+global using FINAL.Services;
+global using FINAL.Models;
